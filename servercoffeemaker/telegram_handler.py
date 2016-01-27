6db443bb6d767c88bb14ef479ce9360d508c2cc0
Kevin Campos <kevin.campos@nkey.com.br>
2016-01-27 21:55:23 -0200

    Telegram handler first version  (っ˘▽˘)っ :envelope: ⊂(◕。◕⊂)

--- servercoffeemaker/telegram_handler.py
+++ servercoffeemaker/telegram_handler.py
@@ -6,16 +6,22 @@ bot = telebot.TeleBot("150255215:AAE-nWXLaP0yArclvR-kjMmIkxUZyCVHE-I")
 
 @bot.message_handler(commands=['querocafe'])
 def send_welcome(message):
-	foo = ['Nope, nope, nope', 'Talvez depois', 'Ok, estara pronto assim que voce me programar', 'Wow, much coffee, very ligado', 'Not enough mana', 'Voce nem bebe cafe.', 'Under construction', 'E a impressora?', 'Tente pelo meu twitter @cafeteira_nkey']
-	bot.reply_to(message, random.choice(foo))
-	print(random.choice(foo))
-	r = requests.get('https://api.github.com/events')
-	print(r.text)
+	if random.randint(0, 1) == 0:
+		foo = ['Nope, nope, nope.', 'Talvez depois.', 'Not enough mana.', 'Voce nem bebe cafe.', 'E a impressora?']
+		bot.reply_to(message, random.choice(foo))
+	else:
+		foo = ['Ok, logo estara pronto.', 'Te aviso quando terminar.', 'Right away, sir.']
+		bot.send_message(-3935030, random.choice(foo))
+		# SERVER: set 1 on global variable #
 
+# Coffee is ready, tell everyone this
+def coffee_is_ready():
+	foo = ['Coffee is ready.']
+	bot.send_message(-3935030, random.choice(foo))
 
 @bot.message_handler(commands=['11h30'])
 def send_welcome(message):
-	foo = ['Miyoshit', 'Pizza hut', 'isRoverThere ? otherPlace : Tradicao', 'Delicious of the field', 'Shopping', '.. nao almoce, tome cafe']
+	foo = ['Miyoshi.', 'Pizza Hut.', '(isRoverThere ? otherPlace : Tradicao)', 'delicious.', 'Shopping.', '.. nao almoce, tome cafe.', 'Five.']
 	bot.reply_to(message, 'Almoce no ' + random.choice(foo))
 	print(random.choice(foo))
 
